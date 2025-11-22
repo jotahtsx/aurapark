a7ec8ab0896140cbdde1dc756cbbe6a0a3579bc6
jotahdev <joaomanoelskai@gmail.com>
2025-11-21 22:46:44 -0300

    Inicializando projeto AuraPark

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>AuraPark - Home</title>
+</head>
+<body>
+    <h1>Bem-vindo ao AuraPark!</h1>
+    <p>Projeto de Estacionamento Laravel em construção.</p>
+</body>
+</html>
